c9c0db2282093209d59e9974fe61cbece9a77337
ghostffcode <meremezeukah@gmail.com>
2016-11-27 17:11:13 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+const co = require('co');
+const prompt = require('co-prompt');
+const program = require('commander');
+const cmd = require('node-cmd');
+
+const github = 'https://api.github.com/user/repos';
+
+let data = {};
+
+function addData (key, value) {
+  if (value !== "undefined" && (value.length > 0 || (typeof value === "boolean"))) {
+    data[key] = value;
+  }
+  return;
+}
+
+program
+  .arguments('<name>')
+  .action(function(name) {
+    co(function *() {
+      addData('name', name);
+      var user = (yield prompt('username: ')).trim();
+      var pass = (yield prompt.password('password: ')).trim();
+      addData('private', (yield prompt('private (y/n)? ', 'n')).toLowerCase().trim() === 'y');
+      addData('auto_init', (yield prompt('Create README.md (y/n)? ')).toLowerCase().trim() === 'y');
+      addData('gitignore_template', (yield prompt('gitignore (Example: Node): ')).trim());
+      addData('license_template', (yield prompt('license (none): ')).trim());
+      var ssh = ((yield prompt('ssh(s) / https(h): ')).toLowerCase().trim() === 's');
+      console.log(data);
+      command = `curl --data '${JSON.stringify(data)}' -X POST -u ${user}:${pass} ${github}`;
+      cmd.get(command, function (data) {
+        data = JSON.parse(data);
+        if (data.id) {
+          console.log("Repo successfully created");
+          let link = (ssh) ? data.ssh_url : data.clone_url;
+          cmd.get(`git init`, function (data) {
+            console.log(data);
+            cmd.run(`git remote add origin `+ link);
+            process.exit(0);
+          });
+        } else {
+          console.log(data.message);
+          process.exit(1);
+        }
+      });
+    });
+  })
+  .parse(process.argv);
